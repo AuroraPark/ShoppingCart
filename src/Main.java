367193a2c854ee1e127dd8bb0da970db821827d7
박현민 <dev.hmpark@gmail.com>
2024-01-03 14:16:49 +0900

    TODO 목록 생성

--- src/Main.java
+++ src/Main.java
@@ -86,6 +86,22 @@ class ShoppingCart {
 
 public class Main {
     public static void main(String[] args) {
+        // 상품 목록 생성
+        Set<Product> productSet = new HashSet<>();
 
+        // TODO: 상품 클래스를 생성하여 상품 목록에 넣는다.
+
+        // 상품 목록 확인
+        System.out.println("고유한 상품 목록: ");
+        for(Product product : productSet){
+            System.out.println(product.getName() + " : " + product.getPrice());
+        }
+
+        // 장바구니 생성
+        ShoppingCart myCart = new ShoppingCart();
+
+        // TODO: 상품을 장바구니에 추가
+        // TODO: 상품을 장바구니에서 제거
+        // TODO: 장바구니에 현재 담긴 상품들을 출력(상품 이름, 각 상품의 갯수)
     }
 }
