b10c5c5c34d2bb5f3f2e6e8c91c762b5e0b9a588
박현민 <dev.hmpark@gmail.com>
2024-01-03 17:21:31 +0900

    csv 파일에서 Product 리스트 가져오기

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
 class Product {
     private String key;
     private String name;
@@ -86,12 +89,33 @@ class ShoppingCart {
 
 public class Main {
     public static void main(String[] args) {
+        // DONE: csv 파일에서 상품 목록 가져오기
+        String csvFile = "src/product.csv";
         // 상품 목록 생성
         Set<Product> productSet = new HashSet<>();
 
-        // TODO: 상품 클래스를 생성하여 상품 목록에 넣는다.
-        productSet.add(new Product("milk", "우유", 2000));
-        productSet.add(new Product("apple", "사과", 1000));
+        try(BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                if(values.length == 3){
+                    String key = values[0].trim();
+                    String name = values[1].trim();
+                    int price = Integer.parseInt(values[2].trim());
+                    productSet.add(new Product(key, name, price));
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("CSV 파일로부터 불러온 상품 목록:");
+        for (Product product : productSet) {
+            System.out.println(product.getName() + " : $" + product.getPrice());
+        }
+        // DONE: 상품 클래스를 생성하여 상품 목록에 넣는다.
+       //  productSet.add(new Product("milk", "우유", 2000));
+       //  productSet.add(new Product("apple", "사과", 1000));
 
         Product milk = new Product("milk", "우유", 2000);
         Product apple = new Product("apple", "사과", 1000);
@@ -105,14 +129,13 @@ public class Main {
         // 장바구니 생성
         ShoppingCart myCart = new ShoppingCart();
 
-        // TODO: 상품을 장바구니에 추가
+        // DONE: 상품을 장바구니에 추가
         myCart.addProduct(milk, 2); // 우유 2개 추가
         myCart.addProduct(apple, 1); // 사과 1개 추가
-
-        // TODO: 상품을 장바구니에서 제거
+        // DONE: 상품을 장바구니에서 제거
         myCart.removeProduct(milk, 1); // 우유 1개 제거
 
-        // TODO: 장바구니에 현재 담긴 상품들을 출력(상품 이름, 각 상품의 갯수)
+        // DONE: 장바구니에 현재 담긴 상품들을 출력(상품 이름, 각 상품의 갯수)
         myCart.showItems();
     }
 }
